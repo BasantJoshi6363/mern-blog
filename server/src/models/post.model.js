15475ebb629cdce03825aa247cdb8219cff45193
BasantJoshi6363 <basantjoshi6363@gmail.com>
2025-07-31 12:08:02 +0545

    feat: add like field to post schema for tracking likes

--- server/src/models/post.model.js
+++ server/src/models/post.model.js
@@ -29,6 +29,10 @@ const postSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "Comment"
     }],
+    like: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Like"
+    }],
 
 }, {
     timestamps: true
