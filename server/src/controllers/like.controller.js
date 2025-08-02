51304c56faa224d1e1f7d6621dc3b989f693df18
BasantJoshi6363 <basantjoshi6363@gmail.com>
2025-08-02 23:52:42 +0545

    fix: update likePost function to handle dislike logic and improve response messages

--- server/src/controllers/like.controller.js
+++ server/src/controllers/like.controller.js
@@ -12,13 +12,22 @@ export const likePost = async (req, res) => {
                 message: "Post not found",
             });
         }
-
+        let liked = ""
         // Check if user already liked this post
         const alreadyLiked = await Like.findOne({ post: id, user: userId });
+        console.log(alreadyLiked);
         if (alreadyLiked) {
-            return res.status(400).json({
-                success: false,
-                message: "User has already liked the post.",
+            await Like.findByIdAndDelete(alreadyLiked._id);
+            await Post.findByIdAndUpdate(id, {
+                $pull: {
+                    like: alreadyLiked._id
+                }
+            })
+
+            return res.status(200).json({
+                success: true,
+                message: "dislike successful",
+                liked: false
             });
         }
 
@@ -36,6 +45,7 @@ export const likePost = async (req, res) => {
             success: true,
             message: "Like created successfully",
             like: createLike,
+            liked: true
         });
     } catch (error) {
         return res.status(500).json({
